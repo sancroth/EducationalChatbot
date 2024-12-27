df5d6fa5e9979424c0cda7b75af304ce470fa2d4
sancroth <sancroth@gmail.com>
2024-12-27 06:39:48 +0200

    final sample data and initdb script

--- sample_data/init_db.py
+++ sample_data/init_db.py
@@ -58,7 +58,7 @@ try:
         id SERIAL PRIMARY KEY,         
         department_id INT NOT NULL,
         email VARCHAR(100) NOT NULL,
-        contact_phone VARCHAR(20) NOT NULL,
+        contact_phone VARCHAR(50) NOT NULL,
         address TEXT,
         working_hours VARCHAR(100),
         website_url VARCHAR(255),
@@ -71,10 +71,10 @@ try:
 
     create_insert_department_secretariats = """
     INSERT INTO department_secretariats (department_id, email, contact_phone, address, working_hours, website_url)
-    VALUES 
-        (1, 'ice@uniwa.gr', '210538-5382, -5308, -5309, -5384', '', 'Δευτ-Τετα-Παρ, 9:00-13:00', 'http://www.ice.uniwa.gr'),
+    VALUES
+        (1, 'ice@uniwa.gr', '210538-5382/5308/5309/5384', '', 'Δευτ-Τετ-Παρ, 9:00-13:00', 'http://www.ice.uniwa.gr'),
         (2, 'alis@uniwa.gr', '2105385203', '', 'Δευτ-Τετ-Παρ, 10:00-14:00', 'http://alis.uniwa.gr')
-    );
+    ;
     """
     cursor.execute(create_insert_department_secretariats)
     conn.commit()
@@ -108,7 +108,7 @@ try:
     	email VARCHAR(100) UNIQUE NOT NULL,
     	date_of_birth DATE,
     	enrollment_date DATE NOT NULL,
-    	department_id INT,
+    	department_id INT NOT NULL,
     	gender VARCHAR(20) NOT NULL,
     	role_id INT NOT NULL DEFAULT '1',
     	CONSTRAINT fk_role_id FOREIGN KEY (role_id) REFERENCES roles(role_id) ON DELETE SET NULL,
@@ -137,8 +137,10 @@ try:
         class_id SERIAL PRIMARY KEY,
         class_name VARCHAR(100) NOT NULL,
         class_type VARCHAR(100) NOT NULL,
+    	department_id INT NOT NULL,
         program VARCHAR(50) NOT NULL DEFAULT 'ΠΡΟΠΤΥΧΙΑΚΟ',
-        semester INT NOT NULL
+        semester INT NOT NULL,
+        CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES departments(id) ON DELETE SET NULL
     );
     """
     cursor.execute(create_classes_query)
@@ -153,10 +155,10 @@ try:
         for row in reader:
             cursor.execute(
                 """
-                INSERT INTO classes (semester,class_name, class_type, program)
-                VALUES (%s, %s, %s, %s);
+                INSERT INTO classes (department_id,semester,class_name, class_type, program)
+                VALUES (%s, %s, %s, %s, %s);
                 """,
-                (row[0], row[1], row[2],row[3])
+                (row[0], row[1], row[2],row[3],row[4])
             )
     conn.commit()
     print("classes.csv imported")
@@ -187,18 +189,69 @@ try:
     '''
     cursor.execute(create_index_query)
 
-    with open('./students.csv','r') as f:
-       next(f)
-       cursor.execute("SET datestyle TO ISO, DMY;")
-       cursor.copy_from(f,"users", sep=',')
-       conn.commit()
+    # SELECT * 
+    # FROM student_info
+    # WHERE special_needs @> '["dyslexia"]';
+    # INSERT INTO student_info (am, semester, special_needs) 
+    # VALUES ('AM1234', 3, '["dyslexia", "hearing_impairment"]');
+    create_student_info_query = '''
+    CREATE TABLE IF NOT EXISTS student_info (
+        id SERIAL PRIMARY KEY,
+        user_id INT UNIQUE NOT NULL,
+        am VARCHAR(50) UNIQUE NOT NULL,
+        semester INT NOT NULL,
+        special_needs JSONB DEFAULT NULL,
+        FOREIGN KEY (user_id) REFERENCES users (user_id)
+    );
+    '''
+    cursor.execute(create_student_info_query)
+    conn.commit()
+    print("Table student_info generated or exists")
+
+    with open('./students.csv', 'r') as f:
+        next(f)
+        reader = csv.reader(f)
+        
+        for row in reader:
+            cursor.execute("SET datestyle TO ISO, DMY;")
+            cursor.execute(
+                """
+                INSERT INTO users (first_name, last_name,email,date_of_birth,enrollment_date,department_id,gender,role_id)
+                VALUES (%s, %s,%s, %s,%s, %s,%s, %s)
+                ON CONFLICT (user_id) DO NOTHING;
+                """,
+                (row[1],row[2],row[3],row[4],row[5],row[6],row[7],row[8])
+            )
+        conn.commit()
+
+        for row in reader:
+            cursor.execute(f"SELECT key FROM departments WHERE id = {row[6]} LIMIT 1")
+            department_key=cursor.fetchone()
+            cursor.execute('''
+                INSERT INTO student_info (user_id, am, semester, special_needs)
+                VALUES (%s, %s, %s,%s)
+                ''',
+                (row[0],f'{department_key[0]}{row[0]}',row[9],None)
+            )
+        conn.commit()
     print("Data from 'students.csv' inserted successfully.")
+    print("Student info generated")
 
     with open('./teachers.csv','r') as f:
-       next(f)
-       cursor.execute("SET datestyle TO ISO, DMY;")
-       cursor.copy_from(f,"users", sep=',')
-       conn.commit()
+        next(f)
+        reader = csv.reader(f)
+        
+        for row in reader:
+            cursor.execute("SET datestyle TO ISO, DMY;")
+            cursor.execute(
+                """
+                INSERT INTO users (first_name, last_name,email,date_of_birth,enrollment_date,department_id,gender,role_id)
+                VALUES (%s, %s,%s, %s,%s, %s,%s, %s)
+                ON CONFLICT (user_id) DO NOTHING;
+                """,
+                (row[1],row[2],row[3],row[4],row[5],row[6],row[7],row[8])
+            )
+    conn.commit()
     print("Data from 'teachers.csv' inserted successfully.")
 
     with open('./students.csv', 'r') as f:
@@ -248,6 +301,9 @@ try:
             # Execute query
             cursor.execute(lookup_teacher_query, (teacher_last_name,))
             teacher_id = cursor.fetchone()
+            if teacher_id==None:
+                print("teacher name missmatch, skipping")
+                continue
             print(f"found for teacher: '{teacher_id[0]}'")
             if teacher_id:
                 teacher_id = teacher_id[0]
@@ -260,6 +316,9 @@ try:
             '''
             cursor.execute(lookup_class_query, (row[1],))
             class_res = cursor.fetchone()
+            if class_res==None:
+                print("class name missmatch, skipping")
+                continue
             print(f"found class id: '{class_res[0]}'")
             if class_res:
                 class_id = class_res[0]
@@ -278,50 +337,19 @@ try:
     conn.commit()
     print("Schedules generated")
 
-
-    # SELECT * 
-    # FROM student_info
-    # WHERE special_needs @> '["dyslexia"]';
-    # INSERT INTO student_info (am, semester, special_needs) 
-    # VALUES ('AM1234', 3, '["dyslexia", "hearing_impairment"]');
-    create_student_info_query = '''
-    CREATE TABLE IF NOT EXISTS student_info (
-        id SERIAL PRIMARY KEY,
-        user_id INT UNIQUE NOT NULL,
-        am VARCHAR(50) UNIQUE NOT NULL,
-        semester INT NOT NULL,
-        special_needs JSONB DEFAULT NULL,
-        FOREIGN KEY (user_id) REFERENCES users (user_id)
-    );
-    '''
-    cursor.execute(create_student_info_query)
-    print("Table student_info generated or exists")
-
-    cursor.execute("SELECT user_id,department_id FROM users WHERE role_id = 1")
-    users = cursor.fetchall()
-
-    for user in users:
-        cursor.execute(f"SELECT key FROM departments WHERE id = {user[1]} LIMIT 1")
-        department_key=cursor.fetchone()
-        cursor.execute('''
-            INSERT INTO student_info (user_id, am, semester, special_needs)
-            VALUES (%s, %s, %s,%s)
-            ''',
-            (user[0],f'{department_key[0]}{user[0]}',1,None)
-        )
-    conn.commit()
-    print("Student info generated")
-
     create_student_enrollments_query = """
     CREATE TABLE IF NOT EXISTS student_enrollments (
         enrollment_id SERIAL PRIMARY KEY,
         user_id INT NOT NULL,
         class_id INT NOT NULL,
         enrollment_date TIMESTAMPTZ DEFAULT NOW(),
+        department_id INT NOT NULL,
         CONSTRAINT fk_user
             FOREIGN KEY (user_id) REFERENCES users (user_id) ON DELETE CASCADE,
         CONSTRAINT fk_class
             FOREIGN KEY (class_id) REFERENCES classes (class_id) ON DELETE CASCADE,
+        CONSTRAINT fk_department
+            FOREIGN KEY (department_id) REFERENCES departments(id) ON DELETE SET NULL,
         UNIQUE (user_id, class_id)
     );"""
 
@@ -329,26 +357,29 @@ try:
     conn.commit()
     print("Table student_enrollments created successfully.")
 
-    for user in users:
-        user_team = 1
-        cursor.execute(f"SELECT semester FROM student_info WHERE user_id = {user[0]};")
-        user_semester=cursor.fetchone()[0]
-        if user_semester == 1:
-            if user[0] % 10 in (7, 8, 9):
-                user_team=2
-        else:
-            if user[0] % 2 == 1:
-                user_team=2
+    with open('./students.csv', 'r') as f:
+        next(f)
+        reader = csv.reader(f)
+        
+        for row in reader:
+            user_team = 1
+            if row[6]==1:
+                if row[9] == 1:
+                    if row[0] % 10 in (7, 8, 9):
+                        user_team=2
+                else:
+                    if row[0] % 2 == 1:
+                        user_team=2
 
-        cursor.execute(f"SELECT class_id FROM classes WHERE semester={user_semester};")
-        classes = cursor.fetchall()
-        for uni_class in classes:
-            cursor.execute('''
-                INSERT INTO student_enrollments (user_id, class_id)
-                VALUES (%s, %s)
-                ''',
-                (user[0],uni_class[0])
-            )
+            cursor.execute(f"SELECT class_id FROM classes WHERE semester={row[9]} and department_id={row[6]};")
+            classes = cursor.fetchall()
+            for uni_class in classes:
+                cursor.execute('''
+                    INSERT INTO student_enrollments (department_id, user_id, class_id)
+                    VALUES (%s, %s, %s)
+                    ''',
+                    (row[6], row[0],uni_class[0])
+                )
     conn.commit()
     print("Student enrollments have been generated")
 
